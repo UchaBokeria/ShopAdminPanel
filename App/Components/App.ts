6ea34298a922e8b58b584d7d940bcdd7a0b77574
UchaBokeria <ucha1bokeria@gmail.com>
2022-02-06 12:00:13 -0800

    x

--- App/Components/App.ts
+++ App/Components/App.ts
@@ -1,15 +1,30 @@
-import { component } from 'riot';
+import { component, mount } from 'riot';
 import { Router, Route } from './Routing.module'
 
 export default class App {
-    name = "Artemenes";
+    title = "Artemida";
+    minimizer = false;
 
-    constructor() {}
+    constructor() {
+        if (localStorage.getItem("minimizer") === null) this.minimizer = false;
+        else this.minimizer = (localStorage.getItem("minimizer") === "yes") ?  true : false;
+    }
+
+    minimize = (e:any) => {
+        document.querySelector(".Side-bar").classList.toggle("Minimize");
+        document.querySelector(".Content").classList.toggle("Content-minimize");
+        document.querySelectorAll(".Title i").forEach((el) => el.classList.toggle("Hide"));
+
+        this.minimizer = !this.minimizer;
+        if(this.minimizer) localStorage.setItem("minimizer", "yes");
+        else localStorage.setItem("minimizer", "no");
+    }
 
     route = (e:any) => {
-        let Comp:string = e.target.getAttribute("Route");
         let Outlet:any = document.querySelector("outlet");
-        
+        let Comp:string = e.srcElement.parentElement.getAttribute("Route");
+        document.querySelectorAll(".Navigation div").forEach((e)=> e.classList.remove("Active"));
+        document.querySelector(".Navigation div[Route='" + Comp + "']").classList.add("Active");
         Route(Comp);
     }
 }
--- App/Components/Routing.module.ts
+++ App/Components/Routing.module.ts
@@ -1,29 +1,47 @@
 import { mount } from 'riot'
 
+export const StartPoint = "Dashboard";
+
 export const Router:any = {
-    
     App: {
         class: async () => await import("./App.ts"),
         style: async () => await import("./App.scss")
     },
-    Home: {
-        class: async () => await import("./Home/Home.ts"),
-        style: async () => await import("./Home/Home.scss")
+    Dashboard: {
+        class: async () => await import("./Dashboard/Dashboard.ts"),
+        style: async () => await import("./Dashboard/Dashboard.scss")
     },
-    About: {
-        class: async () => await import("./About/About.ts"),
-        style: async () => await import("./About/About.scss")
+    Handbook: {
+        class: async () => await import("./Handbook/Handbook.ts"),
+        style: async () => await import("./Handbook/Handbook.scss")
+    },
+    Calendar: {
+        class: async () => await import("./Calendar/Calendar.ts"),
+        style: async () => await import("./Calendar/Calendar.scss")
+    },
+    Archive: {
+        class: async () => await import("./Archive/Archive.ts"),
+        style: async () => await import("./Archive/Archive.scss")
+    },
+    Analytics: {
+        class: async () => await import("./Analytics/Analytics.ts"),
+        style: async () => await import("./Analytics/Analytics.scss")
+    },
+    Settings: {
+        class: async () => await import("./Settings/Settings.ts"),
+        style: async () => await import("./Settings/Settings.scss")
     }
-
 };
 
 export const Route = async (component:any, dest:any = "outlet") => {
-
+    
     let design:any = await Router[component].style();
     let module:any = await Router[component].class();
     let Props:any  = new module.default();
     let Outlet:any = document.querySelector(dest);
 
+    console.log(Outlet);
+    
     Outlet.innerHTML = "";
     mount(Outlet, Props , component);
 
