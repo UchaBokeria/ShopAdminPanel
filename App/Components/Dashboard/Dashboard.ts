558264050e6f0e5881e6b5ec3e8e156868579578
UchaBokeria <ucha1bokeria@gmail.com>
2022-02-10 13:32:34 -0800

    a

---
+++ App/Components/Dashboard/Dashboard.ts
@@ -0,0 +1,87 @@
+export default class Dashboard {
+    name = "Dashboard Component";
+
+    Search() {
+        
+    }
+
+    removeAll() {
+        
+    }
+
+    removeEach() {
+        
+    }
+
+
+    Paging = [1,2,3,4,5];
+
+    Products = [
+        {
+            id: 19423,
+            title: "Fender Squier White Stratocaster",
+            price: 199,
+            status: true,
+            showInStore: true,
+            thumbnail: "https://guitarriego.com/wp-content/uploads/2020/08/Squier-Stratocaster-Classic-Series-70-guitarra-guitarriego-1024x639.jpg"
+        },
+        {
+            id: 29316,
+            title: "Jackson JS32T Black",
+            price: 254,
+            status: false,
+            showInStore: true,
+            thumbnail: "https://d1aeri3ty3izns.cloudfront.net/media/45/457593/600/preview.jpg"
+        },
+        {
+            id: 32349,
+            title: "BC Rich Black",
+            price: 622,
+            status: true,
+            showInStore: false,
+            thumbnail: "https://thumbs.static-thomann.de/thumb/thumb600x600/pics/prod/498349.jpg"
+        },
+        {
+            id: 52331,
+            title: "BC Rich Black",
+            price: 622,
+            status: false,
+            showInStore: false,
+            thumbnail: "https://thumbs.static-thomann.de/thumb/thumb600x600/pics/prod/498349.jpg"
+        },
+
+        {
+            id: 89316,
+            title: "Jackson JS32T Black",
+            price: 254,
+            status: false,
+            showInStore: true,
+            thumbnail: "https://d1aeri3ty3izns.cloudfront.net/media/45/457593/600/preview.jpg"
+        },
+        {
+            id: 99423,
+            title: "Fender Squier White Stratocaster",
+            price: 199,
+            status: true,
+            showInStore: true,
+            thumbnail: "https://guitarriego.com/wp-content/uploads/2020/08/Squier-Stratocaster-Classic-Series-70-guitarra-guitarriego-1024x639.jpg"
+        },
+        {
+            id: 122331,
+            title: "BC Rich Black",
+            price: 622,
+            status: false,
+            showInStore: false,
+            thumbnail: "https://thumbs.static-thomann.de/thumb/thumb600x600/pics/prod/498349.jpg"
+        },
+        {
+            id: 132349,
+            title: "BC Rich Black",
+            price: 622,
+            status: true,
+            showInStore: false,
+            thumbnail: "https://thumbs.static-thomann.de/thumb/thumb600x600/pics/prod/498349.jpg"
+        },
+    ];
+    constructor() {}
+}
